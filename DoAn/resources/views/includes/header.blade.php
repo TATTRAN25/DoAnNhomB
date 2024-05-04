a0f6a7996c71f53a76337269b390c5783e73555d
ThuanEst <vanthuan021102@gmail.com>
2024-05-04 07:31:08 +0700

    create funtional cart details

---
+++ DoAn/resources/views/includes/header.blade.php
@@ -0,0 +1,28 @@
+<header class="header">
+    <nav class="nav">
+        <a href="#" class="nav-logo">
+            <img class="logo-img" src="{{asset('images/logo.png')}}" alt="Logo Shop">
+        </a>
+
+        <div class="nav-menu">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="#" class="nav-link">Trang chủ</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Sản phẩm</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Dịch vụ</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Liên hệ</a></li>
+            </ul>
+        </div>
+
+        <div class="nav-search">
+            <input type="text" placeholder="Tìm kiếm sản phẩm" class="form-input"/>
+            <button class="search-btn">
+                <i class="search-icon fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+
+        <div class="nav-user-action">
+            <a href=""><i class="user-icon fa-solid fa-user"></i></a>
+            <a href=""><i class="cart-icon fa-solid fa-cart-shopping"></i></i></a>
+        </div>
+    </nav>
+</header>
--- DoAn/routes/web.php
+++ DoAn/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products/cart_details', [CartController::class, 'cartDetail'])->name('products.cart');
