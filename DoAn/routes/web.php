97dcc7d031a9bdc5a9febd44472e39b617ba707f
TATTRAN25 <huytran998811@gmail.com>
2024-05-05 23:04:48 +0700

    commid lan 1

--- DoAn/routes/web.php
+++ DoAn/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('MaGiamGia', [UserController::class, 'MaGiamGia'])->name('user.MaGiamGia');
+
+Route::get('ranking', [UserController::class, 'ranking'])->name('user.ranking');
 Route::get('/', function () {
     return view('welcome');
 });
