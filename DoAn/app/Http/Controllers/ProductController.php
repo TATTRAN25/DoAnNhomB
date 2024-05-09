6227858224f2a58a618dc63887a8b81ee73d913b
Tran Anh Tuan <rananhtuan.tdc.2223@gmail.com>
2024-05-09 14:10:51 +0700

    update by tat

---
+++ DoAn/app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class ProductController extends Controller
+{
+    public function productManagement()
+    {
+        return view('product.productmanagement');
+    }
+
+    public function addProduct()
+    {
+        return view('product.addproduct');
+    }
+
+    public function editProduct()
+    {
+        return view('product.editproduct');
+    }
+    
+}
---
+++ DoAn/database.bat
@@ -0,0 +1,4 @@
+php artisan db:seed --class=Categories
+php artisan db:seed --class=DetailUserSeeder
+php artisan db:seed --class=ProductSeeder
+php artisan db:seed --class=UserSeeder
