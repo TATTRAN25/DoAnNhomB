802db20ea0be078022f6dae410cd122b528733cb
dangvanthua <thuandang021102@gmail.com>
2024-05-10 18:28:36 +0700

    add account page

---
+++ DoAn/app/Http/Controllers/CrudUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CrudUserController extends Controller 
+{
+    public function login()
+    {
+        return view('crud.login');
+    }
+
+    public function viewAccountInfo()
+    {
+        return view('crud.account');
+    }
+}
--- DoAn/routes/web.php
+++ DoAn/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CrudUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('login', [CrudUserController::class, 'login'])->name('login');
+Route::get('account', [CrudUserController::class, 'viewAccountInfo'])->name('account');
 
 Route::get('/', function () {
     return view('welcome');
