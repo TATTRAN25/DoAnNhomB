92edfe6f43d5973c40f96b19d6157b5ed2870ea8
TATTRAN25 <huytran998811@gmail.com>
2024-05-10 21:23:58 +0700

    day la comit

--- DoAn/resources/views/product/MaGiamGia.blade.php
+++ DoAn/resources/views/product/MaGiamGia.blade.php
@@ -1,43 +1,22 @@
-<!DOCTYPE html>
-<html lang="vi">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nhom B</title>
-    <link rel="stylesheet" href="{{ asset('css/magiamgia.css')}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-<body>
-    <nav class="navbar">
-        <div class="container">
-            <h1>Quản lý sản phẩm</h1>
-            <div class="search-box">
-                <input type="text" placeholder="Tìm kiếm...">
-                <button type="submit">Tìm kiếm</button>
-            </div>
-            <img class="image" src="{{ asset('images/logo.jpg')}}">
-        </div>
-    </nav>
-
+@section('title', 'Checkout')
+@section('content')
 
     <div class="main">
-        <img class="product-image img" src="{{ asset('images/1694392895130.png')}}" alt="Áo len">
+        <img class="product-image" src="{{ asset('images/1694392895130.png')}}" alt="Áo len">
         <div class="product-info">
-            <h1>Áo len tay lỡ form rộng unisex, Áo len nam nữ form rộng</h1>
-            <p class="price">11.000.000đ - <del>22.000.000</del></p>
-            <p>Ưu đãi hiện tại: <b><i>Giảm giá 50%</i></b></p>
-            <p>Số lượng còn lại: 5024 sản phẩm</p>
-            <p>Mã giảm giá: <input type="text" placeholder="abc-xyz-123"> <button class="btn-discount">Tạo mã giảm giá</button></p>
+            <h1 class="text-h2">Áo len tay lỡ form rộng unisex, Áo len nam nữ form rộng</h1>
+                <div class="text-main">
+                <p class="price">11.000.000đ - <del>22.000.000</del></p>
+                <p>Ưu đãi hiện tại: <b><i>Giảm giá 50%</i></b></p>
+                <p>Số lượng còn lại: 5024 sản phẩm</p>
+                Nhập mã giảm giá: <input type="text"><button>Áp dụng</button>
+            </div>
+
         </div>
     </div>
 
-    <footer class="footer">
-        <div class="container">
-        </div>
-    </footer>
 
-</body>
 
-</html>
+@endsection
--- DoAn/resources/views/product/ranking.blade.php
+++ DoAn/resources/views/product/ranking.blade.php
@@ -1,39 +1,15 @@
-<!DOCTYPE html>
-<html lang="vi">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Áo len tay lỡ form rộng unisex</title>
-    <link rel="stylesheet" href="{{ asset('css/index.css')}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <nav class="navbar">
-        <div class="container">
-            <h1>Quản lý sản phẩm</h1>
-            <div class="search-box">
-                <input type="text" placeholder="Tìm kiếm...">
-                <button type="submit">Tìm kiếm</button>
-            </div>
-            <img class="image" src="{{ asset('images/logo.jpg')}}">
-        </div>
-    </nav>
-
+@section('title', 'Checkout')
+@section('content')
 
     <div class="row">
         <div class="column">
-            <img class="column" width="300px" height="300px" src="{{ asset('images/1694392895130.png')}}" alt="Áo len">
-            <div class="column">
+            <img class="column"  src="{{ asset('images/1694392895130.png')}}" alt="Áo len">
+            <div class="column text-h1">
                 <h1>Áo len tay lỡ form rộng unisex, Áo len nam nữ form rộng</h1>
                 <p class="price">11.000.000đ - <del>22.000.000</del></p>
-            </div>
-        </div>
-
-        <div class="column">
+                <div class="column">
             <h3><b>Đánh giá sản phẩm </b></h3>
             <input class="textbox" type="text" placeholder="Nhập đánh giá ....">
             <div class="button">
@@ -53,14 +29,12 @@
 
             </div>
         </div>
+            </div>
+            
+        </div>
+
+        
 
     </div>
 
-
-    <footer class="footer">
-        <div class="container">
-        </div>
-    </footer>
-</body>
-
-</html>
+    @endsection
