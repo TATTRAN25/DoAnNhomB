d36ea481c9fba1132347ac555a587831bc48cdb5
ThuanEst <vanthuan021102@gmail.com>
2024-05-17 13:48:49 +0700

    add function search into project

--- DoAn/routes/web.php
+++ DoAn/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ProductController::class)->group(function () {
+    Route::get('home', 'home')->name('home');
+    Route::get('products', 'getProducts')->name('products');
+    Route::get('product-detail/{productId}', 'getProductDetail')->name('products.detail');
+    Route::get('product-search', 'search')->name('products.search');
 });
